f7ac7e1c0d9ff73b1b75e5d78b9219eb9e80100f
jaineeshx-bits <jaineesh@decodes.dev>
2026-02-23 13:14:50 +0530

    Test

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const path = require("path")
+const path = require("path");
 // Specify the path to the .env file relative to this server.js file
 require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 const customerRoutes = require("./routes/customerRoutes");
